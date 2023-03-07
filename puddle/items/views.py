05fa41527a74ecb69d053c063848c08922b81a55
jul1998 <jguevara321@gmail.com>
2023-03-06 22:45:56 -0600

    new item form

--- puddle/items/views.py
+++ puddle/items/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 from .models import Item
-
+from .forms import NewItemForm
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=item.pk)[0:3]
@@ -11,3 +11,18 @@ def detail(request, pk):
     return render(request, 'item/detail.html',{
         'item': item, 'related_items': related_items
     })
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+            return redirect('items:detail', pk=item.id,)
+    else:
+        form = NewItemForm()
+    return render(request, 'item/form.html',
+                  {'form': form,
+                   'title': 'New Item'})
